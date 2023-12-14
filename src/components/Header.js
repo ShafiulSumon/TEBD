eac8cd1677232b611a418e63ddd9f238bf5d46ab
Shafiul Alam <shafiulahcb1007@gmail.com>
2023-12-14 23:12:14 +0600

    desining header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,22 @@ import Logo from "../images/logo.png";
 
 function Header() {
 
-    const itemsStyle = "px-10 hover:text-blue-500 font-thin hover:font-bold";
+    const itemsStyle = "px-10 hover:text-blue-500 font-thin hover:border-b-2 border-blue-500 hover:cursor-pointer";
 
     return (
         <div className="flex flex-row justify-between w-full bg-white px-5 text-blue-400">
             {/* <div className="text-2xl font-bold">Trust Engineering BD</div> */}
-            <img src={Logo} alt="T/rust Engineering BD" width={228} height={30} className="pl-10"/>
+            <img src={Logo} alt="Trust Engineering BD" width={228} height={30} className="pl-10"/>
 
             <div className="flex flex-row items-center justify-end w-2/3">
                 <div className={itemsStyle}>Home</div>
+                <div className={itemsStyle}>Producs</div>
                 <div className={itemsStyle}>Contact</div>
+                <div className={itemsStyle}>About Us</div>
             </div>
         </div>
     );
+    
 }
 
 export default Header;
